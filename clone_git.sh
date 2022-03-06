1f3494d79b1ccc5a33b2270c555f15e1c5bbaae0
jamesjamesw <jhdev1418@gmail.com>
2022-03-06 21:03:16 +0000

    Updates [clone|update]_git.sh to be run from anywhere and with list of repositories managed by a file.

--- clone_git.sh
+++ clone_git.sh
@@ -5,14 +5,17 @@ echo Cloning Git Repositories
 # CLI argument for github username
 username=$1
 
-# List of all repositories to clone
-repositories=("tequilatrader" "shellprogramming" "Gruvest" "ostep-code" "TicTacToeBoard" "oracle-operator" "accelerometer-reader" "AVL-HashTable" "galaxy-wallpaper" "playground")
+# Location to clone repositories to
+location=$2
+
+# List of repositories to clone
+repositories=$(cat ./repositories)
 
 for r in ${repositories[@]}
 do
 	echo "-----------------------------------------------------------------------------------------"
 	echo "Cloning $r"
 	echo "..."
-	git clone git@github.com:$username/$r.git
+	git clone git@github.com:$username/$r.git $location/$r
 done
 
--- update_git.sh
+++ update_git.sh
@@ -2,16 +2,18 @@
 
 echo Updating Git Repositories
 
+# Location of git repositories parent directory
+location=$1
+
 # List of all repositories to run 'git pull' on
-repositories=("tequilatrader" "shellprogramming" "Gruvest" "ostep-code" "TicTacToeBoard" "oracle-operator" "accelerometer-reader" "AVL-HashTable" "galaxy-wallpaper" "playground")
+repositories=$(cat ./repositories)
 
 for r in ${repositories[@]}
 do
 	echo "-----------------------------------------------------------------------------------------"
 	echo "Updating $r"
 	echo "..."
-	cd $r
+	cd $location/$r
 	git pull
-	cd ..
 done
 
